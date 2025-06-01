48ec0ea3fd4503fe9a20a20e0ba25639a82807d5
Artas Yaskar <artasyaskar@gmail.com>
2025-06-01 20:41:04 +0500

    Update Ground.js

--- src/scenes/entities/Ground.js
+++ src/scenes/entities/Ground.js
@@ -1,1 +1,16 @@
+import * as THREE from 'three';
+import { scene } from '../utils/Constants';
 
+export function createGround() {
+  const groundGeometry = new THREE.PlaneGeometry(20, 20);
+  const groundMaterial = new THREE.MeshStandardMaterial({
+    color: 0x8c8c8c,
+    roughness: 0.9,
+    metalness: 0.1
+  });
+  const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+  ground.rotation.x = -Math.PI / 2;
+  ground.position.y = 0;
+  ground.receiveShadow = true;
+  scene.add(ground);
+}
