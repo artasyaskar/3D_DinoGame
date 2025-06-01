c02cc6e46bb18833c5d988ba06dbb97fa53a616a
Artas Yaskar <artasyaskar@gmail.com>
2025-06-01 20:42:21 +0500

    Update PhysicsSystem.js

--- src/systems/PhysicsSystem.js
+++ src/systems/PhysicsSystem.js
@@ -1,1 +1,11 @@
+import { getDino, updateDinoPhysics } from '../entities/Dinosaur';
 
+let gameOver = false;
+
+export function setGameOver(status) {
+  gameOver = status;
+}
+
+export function animate(deltaTime) {
+  updateDinoPhysics(deltaTime);
+}
