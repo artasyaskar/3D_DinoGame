50342252c47ce823e4195009d758e1c8234fa4ae
Artas Yaskar <artasyaskar@gmail.com>
2025-06-01 20:42:43 +0500

    Update SoundSystem.js

--- src/systems/SoundSystem.js
+++ src/systems/SoundSystem.js
@@ -1,1 +1,23 @@
+import { Howl } from 'howler';
 
+const sounds = {
+  jump: new Howl({ src: ['/assets/sounds/jump.ogg'] }),
+  crash: new Howl({ src: ['/assets/sounds/crash.ogg'] }),
+  background: new Howl({ 
+    src: ['/assets/sounds/background.mp3'],
+    loop: true,
+    volume: 0.5
+  })
+};
+
+export function playSound(type) {
+  if (sounds[type]) sounds[type].play();
+}
+
+export function stopSound(type) {
+  if (sounds[type]) sounds[type].stop();
+}
+
+export function playBackgroundMusic() {
+  playSound('background');
+}
