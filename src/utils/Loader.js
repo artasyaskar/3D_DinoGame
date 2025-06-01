f97a66b734e65e3cdd04d79e9577762b37bdb7ae
Artas Yaskar <artasyaskar@gmail.com>
2025-06-01 20:43:53 +0500

    Update Loader.js

--- src/utils/Loader.js
+++ src/utils/Loader.js
@@ -1,1 +1,18 @@
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { loadDino } from '../entities/Dinosaur';
+import { createGround } from '../entities/Ground';
 
+let loadedCount = 0;
+const totalAssets = 1;
+
+function onAssetLoad() {
+  loadedCount++;
+  if (loadedCount === totalAssets) {
+    window.dispatchEvent(new Event('assetsLoaded'));
+  }
+}
+
+export function preloadAssets() {
+  createGround(); // Create ground synchronously
+  loadDino(onAssetLoad);
+}
