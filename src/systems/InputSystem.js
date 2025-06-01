6e08c276c9bd25f0ee1679bfa4ec70b2a07449d1
Artas Yaskar <artasyaskar@gmail.com>
2025-06-01 20:41:38 +0500

    Update InputSystem.js

--- src/systems/InputSystem.js
+++ src/systems/InputSystem.js
@@ -1,1 +1,30 @@
+import { jump } from '../entities/Dinosaur';
 
+let gameOver = false;
+
+export function setGameOver(status) {
+  gameOver = status;
+}
+
+function isGameOver() {
+  return gameOver;
+}
+
+export function initInput() {
+  window.addEventListener('keydown', onKeyDown);
+  window.addEventListener('click', onTap);
+  window.addEventListener('touchstart', onTap);
+}
+
+function onKeyDown(e) {
+  if ((e.code === 'Space' || e.code === 'ArrowUp') && !isGameOver()) {
+    jump();
+  }
+}
+
+function onTap(e) {
+  e.preventDefault();
+  if (!isGameOver()) {
+    jump();
+  }
+}
