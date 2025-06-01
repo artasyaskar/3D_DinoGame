2c94e75c4739d26fc0e32b69b81de4c035683afa
Artas Yaskar <artasyaskar@gmail.com>
2025-06-01 20:43:05 +0500

    Update ScoreSystem.js

--- src/systems/ScoreSystem.js
+++ src/systems/ScoreSystem.js
@@ -1,1 +1,24 @@
+let score = 0;
+let highScore = localStorage.getItem('dinoHighScore') || 0;
 
+export function updateScore(delta = 1) {
+  score += delta;
+  document.getElementById('score-display').innerText = `Score: ${Math.floor(score)}`;
+}
+
+export function updateHighScore() {
+  if (score > highScore) {
+    highScore = score;
+    localStorage.setItem('dinoHighScore', highScore);
+  }
+  return highScore;
+}
+
+export function resetScore() {
+  score = 0;
+  updateScore(0);
+}
+
+export function getCurrentScore() {
+  return Math.floor(score);
+}
