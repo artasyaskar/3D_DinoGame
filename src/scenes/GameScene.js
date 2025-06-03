665b77191f58d14c618ed1041e3778bb2fa9544a
Artas Yaskar <artasyaskar@gmail.com>
2025-06-03 10:46:43 +0500

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,2 +1,28 @@
-// This file is intentionally left empty for future scene management
-// Currently all scene setup is in Constants.js
+import { scene, camera, renderer } from '../utils/Constants';
+import { getModel, playSound } from '../utils/Loader';
+import { getDino, updateDinoPhysics } from './entities/Dinosaur';
+import { obstacleManager } from './entities/ObstacleManager';
+
+// This file sets up lights, ground, and adds the dinosaur & obstacles to `scene`
+export function initGameScene() {
+  // Add ground
+  // (Ground.js module already adds ground mesh to `scene` on import)
+  const dino = getDino();
+  scene.add(dino);
+
+  // Add light
+  const light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.set(5, 10, 7.5);
+  light.castShadow = true;
+  scene.add(light);
+
+  // Initialize obstacles
+  obstacleManager.init(scene, camera);
+
+  // Any other setup (fog, skybox) can go here
+}
+
+export function updateGameScene(delta) {
+  updateDinoPhysics(delta);
+  obstacleManager.update(delta);
+}
